023c862c1c54801146f4856fcec9b82445a5f6ed
eresmuyvalioso <eresmuyvalioso@gmail.com>
2020-07-22 21:19:34 +0200

    docs add

---
+++ docs/precache-manifest.e12b7a5fbf21096f6aa2497104300239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b7a8be6ea85573fbec310512d165e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3de9eeea8b0ee8a72ab5",
+    "url": "/static/css/main.65a60256.chunk.css"
+  },
+  {
+    "revision": "71522f13fd6174d0aef1",
+    "url": "/static/js/2.c90e9a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c90e9a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de9eeea8b0ee8a72ab5",
+    "url": "/static/js/main.9b0d9adc.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
